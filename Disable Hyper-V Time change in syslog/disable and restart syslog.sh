416583a1e5c2afcf9d558db04e44f281265ed069
Rudde <13756332+ruddem@users.noreply.github.com>
2020-01-16 14:49:49 +0100

    Fix missing directory

--- Disable Hyper-V Time change in syslog/disable and restart syslog.sh
+++
@@ -1,2 +0,0 @@
-wget https://raw.githubusercontent.com/ruddem/L-O-Scripts/master/Disable%20Hyper-V%20Time%20change%20in%20syslog/15-excludetime.conf /etc/rsyslog.d/
-systemctl restart rsyslog.service
---
+++ Disable Hyper-V Time change in syslog/disable-and-restart-syslog.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/ruddem/L-O-Scripts/master/Disable%20Hyper-V%20Time%20change%20in%20syslog/15-excludetime.conf -O /etc/rsyslog.d/15-excludetime.conf
+systemctl restart rsyslog.service
+
+
+
+
+
+
