d0053657b3fadf347ea58268f8c33c7cf630b482
Rudde <13756332+ruddem@users.noreply.github.com>
2020-01-20 14:29:25 +0100

    Update Disable ServerSignature and ServerToken Apache2.sh

--- Disable ServerSignature and ServerToken Apache2.sh
+++ Disable ServerSignature and ServerToken Apache2.sh
@@ -1,3 +1,3 @@
-echo 'ServerSignature Off' | sudo tee -a /etc/apache2/apache2.conf
-echo 'ServerTokens Prod' | sudo tee -a /etc/apache2/apache2.conf
+echo 'ServerSignature Off' | sudo tee -a /etc/apache2/apache2.conf &&
+echo 'ServerTokens Prod' | sudo tee -a /etc/apache2/apache2.conf &&
 sudo service apache2 restart
